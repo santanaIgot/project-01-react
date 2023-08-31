3c1095210244824d3c6cd4bc20ab8b48d76b0232
santanaIgot <santanaigoor01@gmail.com>
2023-08-30 21:01:02 -0300

    Importando o outlet

--- project-react-02/src/App.jsx
+++ project-react-02/src/App.jsx
@@ -1,22 +1,16 @@
 import React from "react";
 import ListaAlunos from "./componentes/ListaAluno";
 import logoReact from "./assets/react.svg";
+import { Outlet } from "react-router-dom";
 
 // <>
 export default function App() {
 
-  const aluno = 'carlos'
-  const curso = 'ADS'
-  const alunos = ['joel', 'joao', 'smitherson']
-  let altLogo = "logo marca"
+
 
   return(
     <> 
-    <h1>TDSS FIAP - RWD</h1>
-    <p>alunos: {aluno}</p>
-    <p>Curso: {curso}</p>
-
-    <ListaAlunos alunos = {alunos} uriLogoreact = {logoReact} altLogoReact = {altLogo}/>
+    <Outlet/>
     </>
   )
 }
