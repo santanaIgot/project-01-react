c093f796db7db9677d950bd415210faec5de5d30
santanaIgot <santanaigoor01@gmail.com>
2023-08-30 21:00:40 -0300

    Bloco de rotas criado

--- project-react-02/src/main.jsx
+++ project-react-02/src/main.jsx
@@ -3,8 +3,44 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+// BLOCO DE NOTAS
+
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import Home from './routes/home/index.jsx'
+import Produtos from './routes/Produtos/index.jsx'
+import Error from './routes/Error/index.jsx'
+import EditarProdutos from './routes/EditarProdutos/index.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App/>,
+    erroElement:<Error/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+      },
+
+      {
+        path:"/produtos",
+        element:<Produtos/>
+      },
+
+      {
+        path:"/produtos/editar/:id",
+        element:<EditarProdutos/>
+      }
+    ]
+
+  }
+])
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
